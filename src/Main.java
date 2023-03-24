736ce9155b3dfdc11bfa69877226718fb5185812
sdzyryan <sdzyryan@gmail.com>
2023-03-24 20:17:16 +0500

    task2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
     }
 
     public static void task1 () {
@@ -12,7 +17,7 @@ public class Main {
         System.out.println("Значение переменной cat с типом short равно " + cat);
         int dog = 2777643;
         System.out.println("Значение переменной dog с типом int равно " + dog);
-        long cow = 777555666;
+        long cow = 777555666L;
         System.out.println("Значение переменной cow с типом long равно " + cow);
         float tiger = 6.99f;
         System.out.println("Значение переменной tiger с типом float равно " + tiger);
@@ -22,6 +27,39 @@ public class Main {
 
     public static void task2 () {
         System.out.println("Задача 2");
-
+        float a = 27.12f;
+        System.out.println(a);
+        long b = 987678965549L;
+        System.out.println(b);
+        float c = 2.786f;
+        System.out.println(c);
+        int d = 569;
+        System.out.println(d);
+        short e = -159;
+        System.out.println(e);
+        int f = 27897;
+        System.out.println(f);
+        byte g = 67;
+        System.out.println(g);
     }
+    public static void task3 () {
+        System.out.println("Задача 3");
+    }
+
+    public static void task4 () {
+        System.out.println("Задача 4");
+    }
+
+    public static void task5 () {
+        System.out.println("Задача 5");
+    }
+
+    public static void task6 () {
+        System.out.println("Задача 6");
+    }
+
+    public static void task7 () {
+        System.out.println("Задача 7");
+    }
+
 }
