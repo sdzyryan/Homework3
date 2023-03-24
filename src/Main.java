cb216612c4b9e67c6b9610eea54274cdb06c4e0c
sdzyryan <sdzyryan@gmail.com>
2023-03-24 19:57:34 +0500

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        byte mouse = 10;
+        System.out.println("Значение переменной mouse с типом byte равно " + mouse);
+        short cat = 32766;
+        System.out.println("Значение переменной cat с типом short равно " + cat);
+        int dog = 2777643;
+        System.out.println("Значение переменной dog с типом int равно " + dog);
+        long cow = 777555666;
+        System.out.println("Значение переменной cow с типом long равно " + cow);
+        float tiger = 6.99f;
+        System.out.println("Значение переменной tiger с типом float равно " + tiger);
+        double lion = 1.0000044;
+        System.out.println("Значение переменной lion с типом double равно " + lion);
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
     }
 }
